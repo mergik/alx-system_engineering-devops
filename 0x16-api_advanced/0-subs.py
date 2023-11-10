1c309e3747c9e7fd1c5e4b62915ab29c6c4fc9b4
mergik <gmerlix@yahoo.com>
2023-11-10 03:50:59 +0300

    0. How many subs? & Readme

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that queries Reddit API and returns number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return total number of subs on a subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
